83127fdda0fc1f7f1ee3e134936566ec901cf967
Kent <kipd.ph@gmail.com>
2015-07-07 07:55:50 +0800

    add user.php

---
+++ user.php
@@ -0,0 +1,37 @@
+<?php
+
+class User extends CI_Model {
+	
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	public function getUsers($id = FALSE)
+	{
+		if ($id === FALSE) {
+			$query = $this->db->get('users');
+			return $query->result();
+		} else {
+			$query = $this->db->get_where('users', array('ID' => $id));
+			return $query->row_array();
+		}
+	}
+	
+	public function getUserLogin($user_name, $password)
+	{
+		$query = $this->db->get_where('users', array('UserName' => $user_name, 'Password' =>$password));
+		return $query->row_array();
+	}
+	
+	public function createUser($data)
+	{
+		$this->db->insert('users', $data);
+	}
+
+	public function updateUser($data, $id)
+	{
+		$this->db->update('users', $data, 'ID = ' . $id);
+	}
+	
+}
